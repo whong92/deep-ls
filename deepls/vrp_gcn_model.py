658738927d1a7a056a6868fe54e613cef1609a06
ongwaihong <samuelong168@gmail.com>
2024-08-03 12:06:00 +0100

    fix

--- deepls/vrp_gcn_model.py
+++ deepls/vrp_gcn_model.py
@@ -826,7 +826,7 @@ class AverageStateRewardBaselineAgentVRP(BaseAgent):
             for ret, step in zip(returns, experience):
                 state_ids = np.array([state[0].id for state in step['state']])
                 df = pd.DataFrame({'state_ids': state_ids, 'returns': ret.numpy()})
-                avg_ret = np.array(df.groupby('state_ids', as_index=False).returns.transform(np.mean).returns)
+                avg_ret = np.array(df.groupby('state_ids', as_index=False).transform('mean').returns)
                 step['cache']['return'] = ret
                 step['cache']['average_return'] = torch.as_tensor(avg_ret)
             # Perform replay steps:
